6b7566798c4c0c5baa4cc3b1467fb04ae769e457
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-25 11:16:29 -0600

    Generate controller and setup production variables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  get 'welcome_page/welcome'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'welcome_page#welcome'
 end
